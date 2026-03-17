5f9202cc85941d97a1be7cebd0e6ec9094a53274
Álvaro García-Caro Bartolomé <alvgarciacb10@gmail.com>
2026-03-17 11:08:50 +0100

    Initial backend setup with Spring Boot and PostgreSQL

---
+++ thesisplatform/src/main/java/es/upm/tfg/thesisplatform/common/controller/HealthController.java
@@ -0,0 +1,13 @@
+package es.upm.tfg.thesisplatform.common.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public String health() {
+        return "Backend OK";
+    }
+}
---
+++ thesisplatform/src/test/java/es/upm/tfg/thesisplatform/ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.tfg.thesisplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
